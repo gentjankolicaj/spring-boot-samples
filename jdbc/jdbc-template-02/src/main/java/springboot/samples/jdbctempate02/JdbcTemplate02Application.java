843f417c56d58eb824794dbcdee71a8b06ce0525
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-01 22:58:38 +0200

    jdbctemplate-02 : New module.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

---
+++ jdbc/jdbc-template-02/src/main/java/springboot/samples/jdbctempate02/JdbcTemplate02Application.java
@@ -0,0 +1,13 @@
+package springboot.samples.jdbctempate02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplate02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTemplate02Application.class, args);
+	}
+
+}
---
+++ jdbc/jdbc-template-02/src/test/java/springboot/samples/jdbctempate02/JdbcTemplate02ApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.samples.jdbctempate02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplate02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
