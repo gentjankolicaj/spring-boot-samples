aa42813dca9cc14d925e32b5db03ef247b88d9cd
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-02 16:04:49 +0200

    jwt-02: New module.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

---
+++ security/jwt-02/src/main/java/springboot/samples/jwt02/Jwt02Application.java
@@ -0,0 +1,13 @@
+package springboot.samples.jwt02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwt02Application.class, args);
+	}
+
+}
