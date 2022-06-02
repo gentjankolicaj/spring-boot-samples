9593d668a67d8fcadd97b0ead9720ebb23d48704
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-02 13:27:59 +0200

    jdbc-template-02 updates.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

--- jdbc/jdbc-template-02/src/test/java/springboot/samples/jdbctempate02/JdbcTemplate02ApplicationTests.java
+++ jdbc/jdbc-template-02/src/test/java/springboot/samples/jdbctempate02/JdbcTemplate02ApplicationTests.java
@@ -1,9 +1,8 @@
 package springboot.samples.jdbctempate02;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class JdbcTemplate02ApplicationTests {
 
 	@Test
