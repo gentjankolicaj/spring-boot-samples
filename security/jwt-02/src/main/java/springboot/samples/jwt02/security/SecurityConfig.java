01505f5e5ba9d049148922b1dcee54510ed12777
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-03 17:35:23 +0200

    jwt-02: Added access & refresh token after authentication.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

--- security/jwt-02/src/main/java/springboot/samples/jwt02/security/SecurityConfig.java
+++ security/jwt-02/src/main/java/springboot/samples/jwt02/security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package springboot.samples.jwt02.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springboot.samples.jwt02.security.filter.BasicAuthenticationFilter;
 
 
 @Configuration
@@ -27,6 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+       http.csrf().disable();
+       http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+       http.authorizeHttpRequests().anyRequest().permitAll();
+
+       //Create instance of filter & add filter to http
+       http.addFilter(new BasicAuthenticationFilter(authenticationManagerBean()));
+    }
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 }
