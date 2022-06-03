761770e5766db19d1b7070267c088eda16f4defd
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-03 15:58:52 +0200

    jwt-02: Added security package.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

---
+++ security/jwt-02/src/main/java/springboot/samples/jwt02/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package springboot.samples.jwt02.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import springboot.samples.jwt02.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
