bc3a768be845585688e40609b3d7776aed73b4ce
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-05 16:44:26 +0200

    Added module data-mongodb
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

---
+++ data/data-mongodb/src/main/java/springboot/samples/data_mongodb/DataMongoDBApplication.java
@@ -0,0 +1,13 @@
+package springboot.samples.data_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMongoDBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMongoDBApplication.class, args);
+	}
+
+}
---
+++ data/data-mongodb/src/test/java/springboot/samples/data_mongodb/DataMongoDBApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.samples.data_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMongoDBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
