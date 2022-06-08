2fa80085b6ad7eecb10d7689f7bd62be8f3cc0e7
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-08 22:10:09 +0200

    kafka-02: minor fix.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

--- kafka/kafka-02/kafka-02-producer/src/main/java/springboot/samples/kafka02_producer/domain/Coordinate.java
+++ kafka/kafka-02/kafka-02-producer/src/main/java/springboot/samples/kafka02_producer/domain/Coordinate.java
@@ -4,10 +4,12 @@ package springboot.samples.kafka02_producer.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.Serializable;
+
 
 @Data
 @AllArgsConstructor
-public class Coordinate {
+public class Coordinate implements Serializable {
 
     private final Double lat;
     private final Double lon;
