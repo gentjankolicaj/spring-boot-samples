4721db6c1f985e0af6dd45e51e8b5ca78c7355d9
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2023-01-17 17:38:43 +0100

    Added liquibase sample.

---
+++ liquibase/src/main/java/springboot/samples/liquibase/domain/Department.java
@@ -0,0 +1,24 @@
+package springboot.samples.liquibase.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "department")
+public class Department implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long departmentId;
+    private String departmentName;
+    private Long managerId;
+    private Long locationId;
+
+}
---
+++ liquibase/src/main/java/springboot/samples/liquibase/domain/Location.java
@@ -0,0 +1,26 @@
+package springboot.samples.liquibase.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "location")
+public class Location implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long locationId;
+    private String streetAddress;
+    private String postalCode;
+    private String city;
+    private String stateProvince;
+    private Long countryId;
+
+}
