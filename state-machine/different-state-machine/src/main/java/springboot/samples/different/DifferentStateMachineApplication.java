946e1de9141cb53b522fb7d2e0d2a552165a39f3
gentjankolicaj <>
2026-03-27 13:26:10 +0100

    state-machine: added new module 'different-state-machine'

---
+++ state-machine/different-state-machine/src/main/java/springboot/samples/different/DifferentStateMachineApplication.java
@@ -0,0 +1,13 @@
+package springboot.samples.different;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferentStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DifferentStateMachineApplication.class, args);
+    }
+
+}
---
+++ state-machine/different-state-machine/src/test/java/springboot/samples/different/DifferentStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.samples.different;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifferentStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
