be37fbf671205e7110f58fa2737de72258c969cf
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2026-03-30 12:33:47 +0200

    added project-reactor module.

---
+++ project-reactor/base-project-reactor/src/main/java/springboot/samples/baseprojectreactor/BaseProjectReactorApplication.java
@@ -0,0 +1,13 @@
+package springboot.samples.baseprojectreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProjectReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseProjectReactorApplication.class, args);
+    }
+
+}
