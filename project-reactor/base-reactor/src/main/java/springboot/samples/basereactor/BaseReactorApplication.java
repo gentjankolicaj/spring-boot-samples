0bccdbb816e60806232398e09214ab8fde33e6c0
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2026-03-30 15:54:03 +0200

    base-reactor: module updates.

--- project-reactor/base-reactor/src/main/java/springboot/samples/basereactor/BaseReactorApplication.java
+++ project-reactor/base-reactor/src/main/java/springboot/samples/basereactor/BaseReactorApplication.java
@@ -1,13 +1,13 @@
-package springboot.samples.baseprojectreactor;
+package springboot.samples.basereactor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseProjectReactorApplication {
+public class BaseReactorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BaseProjectReactorApplication.class, args);
+        SpringApplication.run(BaseReactorApplication.class, args);
     }
 
 }
