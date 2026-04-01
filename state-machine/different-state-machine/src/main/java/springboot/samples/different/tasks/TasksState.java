93466713f4b0ec2d357173b3fda72c2d5110937c
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2026-04-01 20:46:00 +0200

    different-state-machine: tasks sample.

--- state-machine/different-state-machine/src/main/java/springboot/samples/different/tasks/TasksState.java
+++ state-machine/different-state-machine/src/main/java/springboot/samples/different/tasks/TasksState.java
@@ -2,6 +2,14 @@ package springboot.samples.different.tasks;
 
 public enum TasksState {
 
-  LOCKED, UNLOCKED
+  READY,
+  FORK,
+  TASKS, T1, T2, T3,
+  CHOICE,
+  JOIN,
+  ERROR,
+  AUTOMATIC,
+  MANUAL
+
 
 }
