f753e5f076f10bbc966fa5a3a6293b6b9d2c9feb
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2026-04-02 21:58:21 +0200

    reactive-state-machine: added main.

---
+++ state-machine/reactive-state-machine/src/main/java/springboot/samples/reactivestatemachine/ReactiveStateMachineApplication.java
@@ -0,0 +1,19 @@
+package springboot.samples.reactivestatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author gentjan kolicaj
+ * @since 4/2/26 9:57 PM
+ *
+ */
+@SpringBootApplication
+public class ReactiveStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveStateMachineApplication.class, args);
+    }
+
+}
